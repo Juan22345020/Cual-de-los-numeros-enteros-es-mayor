3bbf7376d1abc76ca9b194bfb88691d4f6e778c5
Juan22345020 <126175834+Juan22345020@users.noreply.github.com>
2023-03-03 17:09:55 -0500

    Add files via upload

---
+++ Cual-de-los-numeros-enteros-es-mayor.py
@@ -0,0 +1,33 @@
+# Cual numero es mayor
+
+print("------------------------------------------")
+print("---------Que numero es mayor--------------")
+print("------------------------------------------")
+
+
+# input
+
+n=int(input("Digite el valor 1 :"))
+m=int(input("Digite el valor 2 :"))
+
+## processing
+
+if m==n:
+    print("los numeros son iguales")
+else:
+    if m>n:
+      print("el numero mayor es: " + str(m))       
+    else:  
+        print("el numero mayor es: " + str(n))
+
+
+
+
+
+
+print("-------------------------------------------")
+print("-----------------Resultados----------------")
+print("-------------------------------------------")
+
+
+
